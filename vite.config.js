a6b49a544c6761f6655da7c698f3f2f984a72212
Adrian Pinto <handshake@adrianpinto.dev>
2023-07-19 11:58:56 +0200

    other workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cdci-test/',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
+  base: '/cdci-test/',
 })
